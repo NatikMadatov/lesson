ed91a675a2927646089df79266cfe43518358488
NatikMadatov <madatovnatik83@gmail.com>
2020-12-14 22:46:05 +0400

    first Git lesson

---
+++ js/script.js
@@ -0,0 +1,23 @@
+
+"use strict";
+
+
+// const apps = 'ivan';
+
+// console.log(`https://youtube.com/${apps}/1`);
+
+
+// const user = "Anar";
+
+// alert(`Hello ${user}`);
+
+// let name = "natik";
+
+// alert (`hello ${name}`);
+
+
+
+
+let numbersOfFilms = prompt("Сколько фильмов вы посмотрели","");
+
+console.log(numbersOfFilms);
