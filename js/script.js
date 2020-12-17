6ac705ae67e4ac1c6a3973cc3233db1c6175d613
NatikMadatov <madatovnatik83@gmail.com>
2020-12-17 18:18:07 +0400

    questions & answers

--- js/script.js
+++ js/script.js
@@ -18,6 +18,29 @@
 
 
 
-let numbersOfFilms = prompt("Сколько фильмов вы посмотрели","");
+const numbersOfFilms = prompt("Сколько фильмов вы посмотрели",'');
 
-console.log(numbersOfFilms);
+
+
+
+
+
+const personalMovieDb = {
+
+    count:numbersOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt('question1?',''),
+        b =prompt('question2?',''),
+        c = prompt('question1?',''),
+        d = prompt('question2','');
+
+
+        personalMovieDb.movies[a] = b;
+        personalMovieDb.movies[c] = d;
+
+        console.log(personalMovieDb);
